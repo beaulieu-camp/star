ad703e039ecf5d9932fe014558ed3290e7aa2c7d
Tom Chauvel <35542432+AquaBx@users.noreply.github.com>
2023-08-07 22:37:38 +0200

    Foutu timezones

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import json
 from io import BytesIO
 from zipfile import ZipFile
 import math
+import pytz
+tz = pytz.timezone('Europe/Paris')
 
 def req_func(url):
     req = requests.get(url)
@@ -49,8 +51,8 @@ for line in myzip.open("calendar.txt").readlines()[1:]:
     l = line.decode('utf-8').replace('"',"").replace('\n',"").split(",")
     jour = int( 6 - math.log2( int( "".join(l[1:8]) , 2 )))
 
-    start = datetime.datetime.strptime(l[8],'%Y%m%d')
-    end = datetime.datetime.strptime(l[9],'%Y%m%d')
+    start = datetime.datetime.strptime(l[8],'%Y%m%d',tz=tz)
+    end = datetime.datetime.strptime(l[9],'%Y%m%d',tz=tz)
     dates = days_from_intervalle( start, end )
     id = l[0]
     calendar[id] = {
@@ -107,7 +109,7 @@ for line in myzip.open("stop_times.txt").readlines()[1:]:
             timestamp = int(date.timestamp())
 
             weakday = date.weekday()
-            if weakday == jour and datetime.datetime.now() < date:
+            if weakday == jour and datetime.datetime.now(tz=tz) < date:
                     
                 out_liste[arretid]["dessertes"][ligneid]["sens"][sens]["horaires"].append(timestamp)
                 out_liste[arretid]["dessertes"][ligneid]["sens"][sens]["horaires"].sort()
